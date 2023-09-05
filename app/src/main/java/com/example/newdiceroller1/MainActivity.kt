08ae009b14d0bd035fc169f622a3aad8735b0c32
Aditya Deshmukh <adityadeshmukh7350@gmail.com>
2023-09-06 00:08:55 +0530

    created function for dise logic

--- app/src/main/java/com/example/newdiceroller1/MainActivity.kt
+++ app/src/main/java/com/example/newdiceroller1/MainActivity.kt
@@ -4,19 +4,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
+import java.util.Random
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val rollButton:Button = findViewById(R.id.roll_button)
 
         rollButton.setOnClickListener{
-            Toast.makeText(this,"button clicked",Toast.LENGTH_LONG).show()
+           rollDise()
         }
 
+    }
+
+    private fun rollDise() {
         val rollResult:TextView = findViewById(R.id.roll_result)
+
+        val randomNumber = Random().nextInt(6)+1;
+        rollResult.text  = randomNumber.toString()
     }
 }
