921fbc3d2d2f9b24b052eb9f42cf128afa157afb
Aditya Deshmukh <adityadeshmukh7350@gmail.com>
2023-09-05 23:20:57 +0530

    init

---
+++ app/src/main/java/com/example/newdiceroller1/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.newdiceroller1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton:Button = findViewById(R.id.roll_button)
+
+        rollButton.setOnClickListener{
+            Toast.makeText(this,"button clicked",Toast.LENGTH_LONG).show()
+        }
+
+        val rollResult:TextView = findViewById(R.id.roll_result)
+    }
+}
