ccc4ef3b297a597fce1bd00a364b1203b8978bdc
Aditya Deshmukh <adityadeshmukh7350@gmail.com>
2023-09-06 22:13:55 +0530

    added dice images

--- app/src/main/java/com/example/newdiceroller1/MainActivity.kt
+++ app/src/main/java/com/example/newdiceroller1/MainActivity.kt
@@ -3,11 +3,15 @@ package com.example.newdiceroller1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import java.util.Random
 
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImg:ImageView;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,10 +23,20 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+//    function to create random numbers
     private fun rollDise() {
-        val rollResult:TextView = findViewById(R.id.roll_result)
-
+        diceImg = findViewById(R.id.dice_img)
         val randomNumber = Random().nextInt(6)+1;
-        rollResult.text  = randomNumber.toString()
+        val dice = when(randomNumber){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+           else -> R.drawable.dice_6
+        }
+        diceImg.setImageResource(dice)
+    var a:Int=0
+    Toast.makeText(this, "Great!! You landed a $randomNumber", Toast.LENGTH_SHORT).show()
     }
 }
